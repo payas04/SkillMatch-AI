bd33bd8a8fed3e5e88e31d901d4d6135d87f3288
Payas Patel <81820285+payas04@users.noreply.github.com>
2026-09-21 19:54:22 +0530

    Implement email verification and password reset

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -2,7 +2,9 @@ import passport from "passport";
 import userModel from "../models/user.model.js";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { configDotenv } from "dotenv";
+
 configDotenv();
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,32 +15,41 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await userModel.findOne({ googleId: profile.id });
+
         if (!user) {
-          user = await userModel.findOne({ email: profile.emails[0].value });
+          user = await userModel.findOne({
+            email: profile.emails[0].value,
+          });
+
           if (user) {
             user.googleId = profile.id;
             user.provider = "google";
+            user.emailVerified = true;
             await user.save();
           } else {
-            // Generate a unique fallback username
             const baseUsername = profile.displayName
               .replace(/\s+/g, "")
               .toLowerCase();
+
             const uniqueSuffix = Math.floor(1000 + Math.random() * 9000);
             const generatedUsername = `${baseUsername}_${uniqueSuffix}`;
+
             user = await userModel.create({
               username: generatedUsername,
               email: profile.emails[0].value,
               googleId: profile.id,
               provider: "google",
+              emailVerified: true,
             });
           }
         }
-        return done(null, user); //pass user to route handler if no error(null)
+
+        return done(null, user);
       } catch (error) {
-        return done(error, null); //pass error
+        return done(error, null);
       }
     },
   ),
 );
+
 export default passport;
