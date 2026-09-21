b60dd005b77e7d889e6e1cc91866904254af32e2
Payas Patel <81820285+payas04@users.noreply.github.com>
2026-09-21 19:55:17 +0530

    Implement email verification and password reset

--- Frontend/src/routes/index.jsx
+++ Frontend/src/routes/index.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter, Navigate } from "react-router";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-
 import Home from "../pages/Home";
 import PublicRoute from "../components/PublicRoute";
 import ProtectedRoute from "../components/ProtectedRoute";
 import Interview from "../pages/Interview";
 import OAuthSucces from "../pages/OAuthSuccess";
+import VerifyEmailPage from "../pages/VerifyEmailPage";
+import ForgotPasswordPage from "../pages/ForgotPasswordPage";
+import ResetPasswordPage from "../pages/ResetPasswordPage";
+
 const router = createBrowserRouter([
-  // Redirect root "/" to "/home"
   {
     path: "/",
     element: <Navigate to="/login" replace />,
@@ -16,35 +18,21 @@ const router = createBrowserRouter([
   {
     element: <ProtectedRoute />,
     children: [
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/interview/:interviewId",
-        element: <Interview />,
-      },
+      { path: "/home", element: <Home /> },
+      { path: "/interview/:interviewId", element: <Interview /> },
     ],
   },
   {
     element: <PublicRoute />,
     children: [
-      {
-        path: "/login",
-        element: <LoginPage />,
-      },
-      {
-        path: "/register",
-        element: <RegisterPage />,
-      },
-      {
-        path: "/oauth-success",
-        element: <OAuthSucces />,
-      },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/register", element: <RegisterPage /> },
+      { path: "/verify-email", element: <VerifyEmailPage /> },
+      { path: "/forgot-password", element: <ForgotPasswordPage /> },
+      { path: "/reset-password", element: <ResetPasswordPage /> },
+      { path: "/oauth-success", element: <OAuthSucces /> },
     ],
   },
-  // 404 Catch-all
-
   {
     path: "*",
     element: <Navigate to="/home" replace />,
