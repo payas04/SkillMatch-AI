e7cee9dd8f6d92a98dbf43d82ba3f3ee9bf4b71e
Payas <payaspatel@gmail.com>
2026-09-23 17:37:31 +0530

    Solving smtp issue

--- Backend/src/utils/transporter.js
+++ Backend/src/utils/transporter.js
@@ -7,6 +7,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4, // <-- forces IPv4, avoids the unreachable IPv6 route
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
